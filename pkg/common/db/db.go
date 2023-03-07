697729d1ef0a668213bcc268ef150f93199649fa
pasly0920 <inwoo920621@gmail.com>
2023-03-07 18:26:20 +0900

    feat: db init 추가

---
+++ pkg/common/db/db.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"log"
+
+	"gin-clean-archi/pkg/common/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Init(url string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&model.User{})
+
+	return db
+}
