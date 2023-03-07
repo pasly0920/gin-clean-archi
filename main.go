24937a19a0c4c7853c6816ffd7fa1f40b3d96ca5
pasly0920 <inwoo920621@gmail.com>
2023-03-07 18:28:01 +0900

    feat: main go 추가

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"gin-clean-archi/pkg/users"
+	"log"
+
+	"gin-clean-archi/pkg/common/config"
+	"gin-clean-archi/pkg/common/db"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	app := fiber.New()
+	db := db.Init(c.DBUrl)
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.Status(fiber.StatusOK).SendString("")
+	})
+
+	users.RegisterRoutes(app, db)
+
+	app.Listen(c.Port)
+}
